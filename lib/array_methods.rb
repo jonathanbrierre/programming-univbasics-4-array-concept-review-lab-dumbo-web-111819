7d71461e12069dde2c87616683101edcbdcfeb4f
jonathan brierre <jonathanbrierre@yahoo.com>
2019-10-15 17:55:04 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,7 +3,9 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  # Add your solution here
+  array.sort!
+  max_value = array.shift
+  p max_value
 end
 
 def find_min_value(array)
