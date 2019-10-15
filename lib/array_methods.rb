f75fad95ed09a4565823c3221d0c3f41d2a1ed9d
jonathan brierre <jonathanbrierre@yahoo.com>
2019-10-15 17:59:10 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,9 +3,7 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  array.sort = arrayx
-  max_value = arrayx[-1]
-  p max_value
+  p array.sort[-1]
 end
 
 def find_min_value(array)
